bbedc9bcac36b4edc0271cbb745b55d8c8ec2434
Hendy Irawan <ceefour666@gmail.com>
2015-05-25 19:48:50 +0700

    project

---
+++ src/main/java/org/opencog/neo4j/OpenCogNeo4jGraphBackingStoreApplication.java
@@ -0,0 +1,12 @@
+package org.opencog.neo4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenCogNeo4jGraphBackingStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenCogNeo4jGraphBackingStoreApplication.class, args);
+    }
+}
