bf9d6893c3531f27d8a8ead6d4ea1fcca363095c
Hendy Irawan <ceefour666@gmail.com>
2015-08-14 18:13:50 +0700

    links using hyperedge, refs #1

--- src/main/java/org/opencog/neo4j/ImportBio5App.java
+++ src/main/java/org/opencog/neo4j/ImportBio5App.java
@@ -204,7 +204,7 @@ public class ImportBio5App implements CommandLineRunner {
     }
 
     public void customNodeToMatch(List<?> concept,
-                                  List<String> outDependencies, Map<String, Object> outParams) {
+                                  Map<String, Object> outParams, List<String> outDependencies) {
         final String typeName = ((Symbol) concept.get(0)).getName();// maintain 1:1 with OpenCog terms - .replaceFirst("Node$", "");
         final String conceptName = (String) concept.get(1);
         final String varName = varNameFor(typeName, concept);
@@ -215,6 +215,144 @@ public class ImportBio5App implements CommandLineRunner {
         outParams.put(nodeNameVar, conceptName);
     }
 
+    /**
+     *
+     * @param relationshipType e.g. {@code rdfs_subClassOf}
+     * @param parts
+     * @param stvStrength
+     * @param stvConfidence
+     * @param stvCount
+     * @param aVarName varName for subject (must be prepared)
+     * @param bVarName varName for subject (must be prepared)
+     * @param outParams
+     * @param outDependencies
+     * @return
+     * @throws Exception
+     */
+    protected CypherPart createBinaryLinkVertex(final String relationshipType,
+                                                CypherParts parts,
+                                                    Double stvStrength,
+                                                    Double stvConfidence,
+                                                    Double stvCount,
+                                                    String aVarName, String bVarName,
+                                                    Map<String, Object> outParams, List<String> outDependencies) throws Exception {
+        final String varName = relationshipType + "_" + RandomUtils.nextInt(1000, 10000);
+        String create;
+        if (stvStrength != null) {
+            create = String.format("CREATE (%s:%s {gid: {gid}, tv: {tv}})",
+                    varName, relationshipType);
+        } else {
+            create = String.format("CREATE (%s:%s {gid: {gid}})", varName, relationshipType);
+        }
+        outParams.put("gid", RANDOM.nextLong());
+        outParams.put("tv", new double[] {
+                Optional.ofNullable(stvStrength).orElse(0d), Optional.ofNullable(stvConfidence).orElse(0d),
+                Optional.ofNullable(stvCount).orElse(0d)});
+        // RDF reification, rdf:subject and rdf:object
+        create += String.format("\n  CREATE (%s) -[:rdf_subject]-> (%s)", varName, aVarName);
+        create += String.format("\n  CREATE (%s) -[:rdf_object]-> (%s)", varName, bVarName);
+
+        final CypherPart part = new CypherPart(null, ImmutableMap.copyOf(outParams), create, ImmutableList.copyOf(outDependencies));
+        parts.relationships.add(part);
+        return part;
+    }
+
+    /**
+     *
+     * @param parts
+     * @param stvStrength
+     * @param stvConfidence
+     * @param stvCount
+     * @param predicateName
+     * @param predicateType
+     * @param predicateVarName
+     * @param outParams
+     * @param outDependencies
+     * @return
+     * @throws Exception
+     */
+    protected CypherPart createEvaluationLinkVertex(CypherParts parts,
+                                                    Double stvStrength,
+                                                    Double stvConfidence,
+                                                    Double stvCount,
+                                                    String predicateName,
+                                                    String predicateType,
+                                                    String predicateVarName,
+                                                    List<String> paramNames,
+                                                    Map<String, Object> outParams, List<String> outDependencies) throws Exception {
+        final String relationshipType = "opencog_EvaluationLink";
+        final String varName = relationshipType + "_" + predicateName + "_" + RandomUtils.nextInt(1000, 10000);
+        String create;
+        if (stvStrength != null) {
+            create = String.format("CREATE (%s:%s {gid: {gid}, tv: {tv}})",
+                    varName, relationshipType);
+        } else {
+            create = String.format("CREATE (%s:%s {gid: {gid}})", varName, relationshipType);
+        }
+        outParams.put("gid", RANDOM.nextLong());
+        outParams.put("tv", new double[] {
+                Optional.ofNullable(stvStrength).orElse(0d), Optional.ofNullable(stvConfidence).orElse(0d),
+                Optional.ofNullable(stvCount).orElse(0d)});
+        create += String.format("\n  CREATE (%s) -[:opencog_predicate]-> (%s)", varName, predicateVarName);
+        // http://schema.org/position
+        for (int i = 0; i < paramNames.size(); i++) {
+            create += String.format("\n  CREATE (%s) -[:opencog_parameter {position: %d}]-> (%s)",
+                    varName, i, paramNames.get(i));
+        }
+
+        final CypherPart part = new CypherPart(null, ImmutableMap.copyOf(outParams), create, ImmutableList.copyOf(outDependencies));
+        parts.relationships.add(part);
+        return part;
+    }
+
+    /**
+     *
+     * @param parts
+     * @param top
+     * @return
+     * @deprecated For OpenCog, all Links are using hyperedges now.
+     */
+    @Deprecated
+    public CypherPart inheritanceToCypherRelationship(CypherParts parts, List<?> top) {
+        final ArrayList<String> outDependencies = new ArrayList<>();
+        final Map<String, Object> outParams = new HashMap<>();
+
+        // ensure a is prepared
+        final List<?> a = (List<?>) top.get(1);
+        final String aType = typeNameFor(a);
+        final String aVarName = varNameFor(aType, a);
+        if (!parts.nodes.containsKey(aVarName)) {
+            final CypherPart aCypher = customNodeToCypher(a);
+            parts.nodes.put(aVarName, aCypher);
+        }
+        customNodeToMatch(a, outParams, outDependencies);
+
+        // ensure b is prepared
+        final List<?> b = (List<?>) top.get(2);
+        final String bType = typeNameFor(b);
+        final String bVarName = varNameFor(bType, b);
+        if (!parts.nodes.containsKey(bVarName)) {
+            final CypherPart bCypher = customNodeToCypher(b);
+            parts.nodes.put(bVarName, bCypher);
+        }
+        customNodeToMatch(b, outParams, outDependencies);
+
+        final String create = String.format("CREATE (%s) -[:rdfs_subClassOf {%s: {gid}}]-> (%s)",
+                aVarName, Atom.GID_PROPERTY, bVarName);
+        outParams.put("gid", RANDOM.nextLong());
+        final CypherPart part = new CypherPart(null, ImmutableMap.copyOf(outParams), create,
+                ImmutableList.copyOf(outDependencies));
+        parts.relationships.add(part);
+        return part;
+    }
+
+    /**
+     * (InheritanceLink a b)
+     * means: a rdfs:subClassOf b
+     * @param parts
+     * @param top
+     * @return
+     */
     public CypherPart inheritanceToCypher(CypherParts parts, List<?> top) {
         final ArrayList<String> outDependencies = new ArrayList<>();
         final Map<String, Object> outParams = new HashMap<>();
@@ -227,7 +365,7 @@ public class ImportBio5App implements CommandLineRunner {
             final CypherPart aCypher = customNodeToCypher(a);
             parts.nodes.put(aVarName, aCypher);
         }
-        customNodeToMatch(a, outDependencies, outParams);
+        customNodeToMatch(a, outParams, outDependencies);
 
         // ensure b is prepared
         final List<?> b = (List<?>) top.get(2);
@@ -237,15 +375,14 @@ public class ImportBio5App implements CommandLineRunner {
             final CypherPart bCypher = customNodeToCypher(b);
             parts.nodes.put(bVarName, bCypher);
         }
-        customNodeToMatch(b, outDependencies, outParams);
+        customNodeToMatch(b, outParams, outDependencies);
 
-        final String create = String.format("CREATE (%s) -[:rdfs_subClassOf {%s: {gid}}]-> (%s)",
-                aVarName, Atom.GID_PROPERTY, bVarName);
-        outParams.put("gid", RANDOM.nextLong());
-        final CypherPart part = new CypherPart(null, ImmutableMap.copyOf(outParams), create,
-                ImmutableList.copyOf(outDependencies));
-        parts.relationships.add(part);
-        return part;
+        try {
+            return createBinaryLinkVertex("rdfs_subClassOf", parts, null, null, null,
+                    aVarName, bVarName, outParams, outDependencies);
+        } catch (Exception e) {
+            throw new RuntimeException("Cannot create CypherPart for " + top, e);
+        }
     }
 
     /**
@@ -295,7 +432,7 @@ public class ImportBio5App implements CommandLineRunner {
             final CypherPart predicateCypher = customNodeToCypher(predicate);
             parts.nodes.put(predicateVarName, predicateCypher);
         }
-        customNodeToMatch(predicate, outDependencies, outParams);
+        customNodeToMatch(predicate, outParams, outDependencies);
 
         // ensure all params are prepared
         try {
@@ -309,32 +446,84 @@ public class ImportBio5App implements CommandLineRunner {
                     final CypherPart paramCypher = customNodeToCypher(param);
                     parts.nodes.put(paramVarName, paramCypher);
                 }
-                customNodeToMatch(param, outDependencies, outParams);
+                customNodeToMatch(param, outParams, outDependencies);
 
                 paramNames.add(paramVarName);
             }
 
-            final String varName = "EvaluationLink_" + predicateName + "_" + RandomUtils.nextInt(1000, 10000);
-            String create;
-            if (stvStrength != null) {
-                create = String.format("CREATE (%s:opencog_EvaluationLink {gid: {gid}, stv_strength: %f, stv_confidence: %f})",
-                        varName, stvStrength, stvConfidence);
+            return createEvaluationLinkVertex(parts, stvStrength, stvConfidence, null,
+                    predicateName, predicateType, predicateVarName,
+                    paramNames, outParams, outDependencies);
+        } catch (Exception e) {
+            throw new RuntimeException("Cannot create CypherPart for " + top, e);
+        }
+    }
+
+    /**
+     * Either:
+     *
+     * <ol>
+     *     <li><code>(MemberLink (GeneNode "A0A087WZ62") (ConceptNode "GO:0004826"))</code></li>
+     *     <li><code>(MemberLink (stv 0.0 1.0) (GeneNode "A0A087WZ62") (ConceptNode "GO:0004826"))</code></li>
+     * </ol>
+     *
+     * @param parts
+     * @param top
+     * @return
+     * @deprecated For OpenCog, all {@link org.opencog.atomspace.Link}s now use hyperedge.
+     */
+    @Deprecated
+    public CypherPart memberToCypherAsRelationship(CypherParts parts, List<?> top) {
+        try {
+            final ArrayList<String> outDependencies = new ArrayList<>();
+            final LinkedHashMap<String, Object> outParams = new LinkedHashMap<>();
+
+            @Nullable
+            Double stvStrength = null;
+            @Nullable
+            Double stvConfidence = null;
+            final List<?> stvMaybe = (List<?>) top.get(1);
+            final int memberOffset;
+            if ("stv".equals(((Symbol) stvMaybe.get(0)).getName())) {
+                memberOffset = 2;
+                stvStrength = (Double) stvMaybe.get(1);
+                stvConfidence = (Double) stvMaybe.get(2);
             } else {
-                create = String.format("CREATE (%s:opencog_EvaluationLink {gid: {gid}})", varName);
-            }
-            outParams.put("gid", RANDOM.nextLong());
-            create += String.format("\n  CREATE (%s) -[:opencog_predicate]-> (%s)", varName, predicateVarName);
-            // http://schema.org/position
-            for (int i = 0; i < paramNames.size(); i++) {
-                create += String.format("\n  CREATE (%s) -[:opencog_parameter {position: %d}]-> (%s)",
-                        varName, i, paramNames.get(i));
+                memberOffset = 1;
             }
 
-            final CypherPart part = new CypherPart(null, ImmutableMap.copyOf(outParams), create, ImmutableList.copyOf(outDependencies));
+            // ensure gene is prepared
+            final List<?> gene = (List<?>) top.get(memberOffset);
+            final String geneVarName = varNameForGene(gene);
+            if (!parts.nodes.containsKey(geneVarName)) {
+                final CypherPart geneCypher = customNodeToCypher(gene);
+                parts.nodes.put(geneVarName, geneCypher);
+            }
+            customNodeToMatch(gene, outParams, outDependencies);
+
+            // ensure concept is prepared
+            final List<?> concept = (List<?>) top.get(memberOffset + 1);
+            final String conceptVarName = varNameForConcept(concept);
+            if (!parts.nodes.containsKey(conceptVarName)) {
+                final CypherPart conceptCypher = customNodeToCypher(concept);
+                parts.nodes.put(conceptVarName, conceptCypher);
+            }
+            customNodeToMatch(concept, outParams, outDependencies);
+
+            final String create;
+            if (stvStrength != null) {
+                create = String.format("CREATE (%s) -[:rdf_type {gid: {gid}, stv_strength: %f, stv_confidence: %f}]-> (%s)",
+                        geneVarName, stvStrength, stvConfidence, conceptVarName);
+            } else {
+                create = String.format("CREATE (%s) -[:rdf_type {gid: {gid}}]-> (%s)", geneVarName, conceptVarName);
+            }
+            outParams.put("gid", RANDOM.nextLong());
+            final CypherPart part = new CypherPart(null, ImmutableMap.copyOf(outParams), create,
+                    ImmutableList.copyOf(outDependencies));
             parts.relationships.add(part);
             return part;
         } catch (Exception e) {
-            throw new RuntimeException("Cannot create CypherPart for " + top, e);
+            throw new RuntimeException("Cannot create CypherParts for MemberLink " + top, e);
         }
     }
 
@@ -376,7 +565,7 @@ public class ImportBio5App implements CommandLineRunner {
                 final CypherPart geneCypher = customNodeToCypher(gene);
                 parts.nodes.put(geneVarName, geneCypher);
             }
-            customNodeToMatch(gene, outDependencies, outParams);
+            customNodeToMatch(gene, outParams, outDependencies);
 
             // ensure concept is prepared
             final List<?> concept = (List<?>) top.get(memberOffset + 1);
@@ -385,20 +574,11 @@ public class ImportBio5App implements CommandLineRunner {
                 final CypherPart conceptCypher = customNodeToCypher(concept);
                 parts.nodes.put(conceptVarName, conceptCypher);
             }
-            customNodeToMatch(concept, outDependencies, outParams);
+            customNodeToMatch(concept, outParams, outDependencies);
 
-            final String create;
-            if (stvStrength != null) {
-                create = String.format("CREATE (%s) -[:rdf_type {gid: {gid}, stv_strength: %f, stv_confidence: %f}]-> (%s)",
-                        geneVarName, stvStrength, stvConfidence, conceptVarName);
-            } else {
-                create = String.format("CREATE (%s) -[:rdf_type {gid: {gid}}]-> (%s)", geneVarName, conceptVarName);
-            }
-            outParams.put("gid", RANDOM.nextLong());
-            final CypherPart part = new CypherPart(null, ImmutableMap.copyOf(outParams), create,
-                    ImmutableList.copyOf(outDependencies));
-            parts.relationships.add(part);
-            return part;
+            return createBinaryLinkVertex("rdf_type", parts,
+                    stvStrength, stvConfidence, null,
+                    geneVarName, conceptVarName, outParams, outDependencies);
         } catch (Exception e) {
             throw new RuntimeException("Cannot create CypherParts for MemberLink " + top, e);
         }
